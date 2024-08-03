09008e353b3f9dc07a740b562c044114b592d2a1
Girish <girish@post.com>
2024-08-03 13:04:13 -0700

    QuitCommand test

---
+++ FlixOne/FlixOne.InventoryManagementClient/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Reflection;
+
+Greeting();
+
+
+
+// note: inline out variable introduced as part of C# 7.0
+//GetCommand("?").RunCommand(out bool shouldQuit);
+
+
+
+//while (!shouldQuit)
+//{
+//    // handle the commands
+//}
+
+Console.WriteLine("CatalogService  has completed.");
+
+void Greeting()
+{
+    var version = Assembly.GetExecutingAssembly().GetName().Version;
+    Console.WriteLine("***********************************");
+    Console.WriteLine("*                                  *");
+    Console.WriteLine("*     Welcome to FlixOne Inventory Management System");
+    Console.WriteLine($"*                           v{version}       *");
+    Console.WriteLine("***********************************");
+    Console.WriteLine("");
+
+}
+
+object GetCommand(string v)
+{
+    throw new NotImplementedException();
+}
---
+++ FlixOne/FlixOne.InventoryManagementTests/AddInventoryCommandTests.cs
@@ -0,0 +1,14 @@
+namespace FlixOne.InventoryManagementTests;
+
+[TestClass]
+public class AddInventoryCommandTests
+{
+    [TestMethod]
+    public void AddInventoryCommand_Successful()
+    {
+        // create an instance of the command
+        // add a new book with parameter 'name'
+        // verify the book was added with the given name with 0 quantity
+        Assert.Inconclusive("AddInventoryComman_Successful has not been implemented");
+    }
+}
